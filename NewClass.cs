8ec68746ea800eaab199ec7b6873a8d2e41aeebe
LanaBitanga <lbitan00@fesb.hr>
2026-03-30 15:59:43 +0200

    Update NewClass.cs
    promjena napravljena preko weba

--- NewClass.cs
+++ NewClass.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace vjezba1
 {
-    internal class NewClass
+    public class NewClass
     {
+      
+        public void MojaDrugaMetoda()
+        {
+            // prazna metoda
+        }
     }
 }
-//druga promjena
